0680268894032162c7313754a357f75616bb13c7
frnode <gp.corentin@gmail.com>
2019-09-04 15:05:03 +0200

    Rework machine_vbox.py

--- automanagemachine/components/machine/machine_vbox.py
+++ automanagemachine/components/machine/machine_vbox.py
@@ -2,13 +2,10 @@
 # coding: utf-8
 import datetime
 import os
-import time
-
 import virtualbox
-from virtualbox.library import OleErrorInvalidarg, VBoxErrorFileError, VBoxErrorInvalidObjectState, \
-    VBoxErrorObjectNotFound, VBoxErrorNotSupported, VBoxErrorIprtError, VBoxErrorXmlError, OleErrorAccessdenied, \
-    OleErrorUnexpected, VBoxErrorVmError, VBoxErrorObjectInUse, VBoxErrorInvalidVmState, IUnattended
-from virtualbox.library_ext import IMachine
+from virtualbox.library import VBoxErrorIprtError, VBoxErrorObjectNotFound, OleErrorUnexpected, OleErrorInvalidarg, \
+    VBoxErrorInvalidObjectState, VBoxErrorVmError, VBoxErrorMaximumReached, VBoxErrorFileError, VBoxErrorXmlError, \
+    OleErrorAccessdenied
 
 from automanagemachine.components import utils
 from automanagemachine.components.machine.machine import Machine
@@ -17,7 +14,7 @@ from automanagemachine.core import cfg, logger
 
 class MachineVbox(Machine):
     """
-    TODO
+    Class inherited from Machine, it is used to initiate / conduct actions on a virtualbox machine
     """
 
     def __init__(self):
@@ -33,7 +30,8 @@ class MachineVbox(Machine):
 
     def __create_with_ova(self):
         """
-        Create VM with .OVA file
+        Create a virtual machine under virtualbox using a .OVA file
+        The name of the OVA appliance will be modified using the configuration file
         """
         __ova_file = os.getcwd() + "/data/ova/" + self.ova
 
@@ -43,11 +41,18 @@ class MachineVbox(Machine):
         __appliance = self.vbox.create_appliance()
         __progress = __appliance.read(__ova_file)
         logger.info("Reading the .ova file...")
-        __progress.wait_for_completion(-1)
+
+        try:
+            __progress.wait_for_completion(-1)
+        except VBoxErrorIprtError:
+            logger.warning("Can not read .OVA file: " + __ova_file)
+            utils.stop_program()
+
         __appliance.interpret()
 
         if __appliance.get_warnings() is not None:
             logger.warning(__appliance.get_warnings())
+            utils.stop_program()
 
         __desc = __appliance.find_description(self.ova_appliance_name)
 
@@ -64,28 +69,53 @@ class MachineVbox(Machine):
         __desc.set_memory(self.virtual_memory)
 
         __progress = __appliance.import_machines()
-        logger.info("Creation in progress, wait...")
-        __progress.wait_for_completion(-1)
+        logger.info("Creation/import in progress, wait...")
+
+        try:
+            __progress.wait_for_completion(-1)
+        except VBoxErrorIprtError:
+            logger.warning("Can not import the machine: " + __ova_file)
+            utils.stop_program()
+
         logger.info("Machine created: " + self.name)
 
     def __modify(self):
         """
-        Edit the created machine
+        Edit the created machine to set parameters
         """
         __vm = self.vbox.find_machine(self.name)
-        __session = __vm.create_session()
+
+        try:
+            __session = __vm.create_session()
+        except VBoxErrorIprtError:
+            logger.warning("Error creating guest session")
+            utils.stop_program()
+        except VBoxErrorMaximumReached:
+            logger.warning("The maximum of concurrent guest sessions has been reached")
+            utils.stop_program()
 
         __session.machine.cpu_execution_cap = int(self.cpu_execution_cap)
         __session.machine.memory_balloon_size = int(self.memory_balloon_size)
-        __session.machine.description = "Created with " + cfg['app']['name'] + " on the " + datetime.datetime.now().\
+        __session.machine.description = "Created with " + cfg['app']['name'] + " on the " + datetime.datetime.now(). \
             strftime('%Y-%m-%d at %H:%M:%S.%f') + "\nBased on the appliance: " + self.ova_appliance_name
 
-        __session.machine.save_settings()
+        try:
+            __session.machine.save_settings()
+        except VBoxErrorFileError:
+            logger.warning("Settings file not accessible")
+            utils.stop_program()
+        except VBoxErrorXmlError:
+            logger.warning("Could not parse the settings file")
+            utils.stop_program()
+        except OleErrorAccessdenied:
+            logger.warning(" Modification request refused")
+            utils.stop_program()
+
         __session.unlock_machine()
 
     def create(self):
         """
-        Create machine
+        Create a machine under virtualbox
         """
         Machine.create(self)
         self.__create_with_ova()
@@ -93,17 +123,44 @@ class MachineVbox(Machine):
 
     def run(self):
         """
-        Launch machine machine
+        Start a machine under virtualbox
         """
         logger.info("Starting the machine: " + self.name)
         __session = virtualbox.Session()
-        __vm = self.vbox.find_machine(self.name)
-        __progress = __vm.launch_vm_process(__session, 'gui', '')
-        __progress.wait_for_completion()
+        __vm = self.__find_vm_by_uuid_or_name(self.name)
+
+        try:
+            __progress = __vm.launch_vm_process(__session, 'gui', '')
+        except OleErrorUnexpected:
+            logger.warning("Virtual machine not registered: " + self.name)
+            utils.stop_program()
+        except OleErrorInvalidarg:
+            logger.warning("The session is invalid")
+            utils.stop_program()
+        except VBoxErrorObjectNotFound:
+            logger.warning("The machine was not found: " + self.name)
+            utils.stop_program()
+        except VBoxErrorInvalidObjectState:
+            logger.warning("The machine is already on or is being switched on")
+            utils.stop_program()
+        except VBoxErrorIprtError:
+            logger.warning("Can not start the machine")
+            utils.stop_program()
+        except VBoxErrorVmError:
+            logger.warning("Failed to assign machine to session")
+            utils.stop_program()
+
+        try:
+            __progress.wait_for_completion(-1)
+        except VBoxErrorIprtError:
+            logger.warning("Can not start the machine")
+            utils.stop_program()
+
         logger.info("Machine started")
 
     def __exist(self, name):
         """
+        Check if a machine exists
         :return: Bool
         :param name: Name of machine
         """
@@ -126,7 +183,11 @@ class MachineVbox(Machine):
 
         return __generated_name
 
-    def __get_vm_by_uuid(self, uuid):
-        __vbox = virtualbox.VirtualBox()
-        __vm = __vbox.find_machine(uuid)
+    def __find_vm_by_uuid_or_name(self, uuid):
+        try:
+            __vm = self.vbox.find_machine(uuid)
+        except VBoxErrorObjectNotFound:
+            logger.warning("Can not find the machine with the name/uuid: " + uuid)
+            utils.stop_program()
+
         return __vm
