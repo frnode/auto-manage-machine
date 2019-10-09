dad37502d0a1a605173e8c1aa3f26d7b553d94bc
frnode <gp.corentin@gmail.com>
2019-10-09 13:51:46 +0200

    Update deb_demo.py - Adding comments - Rewrite subprocess_run, problem with characters of symbol types, solution found: remove via remove_specials_characters - Add print_console function - Update Apache2 class -- Add/update install, reload, enable_conf, __create_dir, __set_permissions, put_default_index, create_vhost, create - End of script result reminding IPs and user session

--- automanagemachine/data/scripts/deb_demo.py
+++ automanagemachine/data/scripts/deb_demo.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
 import subprocess
 import os
 import importlib
+import sys
 import uuid
 import tempfile
 import re
 
 WORKDIR = os.getcwd()
 URL_SSH_PUB_KEY = "https://gist.githubusercontent.com/frnode/681d838e61ff579e935eec1ac910a226/raw/OC_P5_RSA_PUB_KEY.pub"
-SSH_USER = "amm2"
+SSH_USER = "amm"
 
 
 def super_pip(packages, retry=False):
     """
-    TODO
-    :param retry:
-    :param packages:
+    Install the defined pip packages
+    :param packages: List of packages to install
+    :param retry: True: If the function is started from itself
     """
     import pip
 
     if retry:
+        print_console("Import pip package: " + packages)
         globals()[packages] = importlib.import_module(packages)
     else:
         for package in packages:
+            print_console("Install/import pip package: " + package)
             try:
                 globals()[package] = importlib.import_module(package)
             except ImportError:
@@ -36,21 +38,48 @@ def super_pip(packages, retry=False):
                 super_pip(package, retry=True)
 
 
-def subprocess_run(args):
+def subprocess_run(args, wait=True):
     """
-    Run command and return Stdout or Strerr
+    Run command and return stdout or strerr
+    :param wait: Wait
+    :param args: List containing the command and arguments
+    :return: Return code
     """
     try:
-        process = subprocess.run(args, check=True, universal_newlines=True, stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        print(e.stderr)
-    else:
-        if process.returncode == 0:
-            # TODO: Use log
-            print(process.stdout)
+        p = subprocess.Popen(
+            args,
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if wait:
+            p.wait()
 
-    return process.returncode
+        stdout, stderr = p.communicate()
+    except subprocess.CalledProcessError as e:
+        lines = stderr.decode('utf-8').splitlines()
+        for line in lines:
+            line = remove_specials_characters(line)
+            print(line, file=sys.stderr)
+
+    if p.returncode == 0:
+        lines = stdout.decode('utf-8').splitlines()
+        for line in lines:
+            line = remove_specials_characters(line)
+            print(line, file=sys.stdout)
+
+    return p.returncode
+
+
+def remove_specials_characters(str):
+    """
+    Remove annoying characters for display in the console
+    :param str: Character string to clean
+    :return: Character string cleaned
+    """
+    str = re.sub(r"[^a-zA-Z0-9 .,/()<>+=@%!\"#?'~$^&*:;[\]_\\-]", ' ', str)
+    return str
+
+
+def print_console(text):
+    print("*::: " + text + " :::*")
 
 
 def generate_random_str(length=6):
@@ -64,23 +93,23 @@ def generate_random_str(length=6):
 
 class User:
     """
-    TODO
+    Manage the user
     """
 
     def __init__(self, user, password=None):
         self.user = user
         self.password = password
-        pass
 
     def create(self):
         """
-        TODO
+        Create a user
         """
         import pwd
 
         try:
             pwd.getpwnam(self.user)
         except KeyError:
+            print_console("User creation: " + self.user)
             __params = ["adduser"]
 
             # If the password is blank, disable it
@@ -98,7 +127,7 @@ class User:
 
 class SSHAuthorizedKeys:
     """
-    TODO
+    Manage SSH keys
     """
 
     def __init__(self, user, key_url):
@@ -108,7 +137,7 @@ class SSHAuthorizedKeys:
 
     def create_dir_and_file(self):
         """
-        TODO
+        Create the directories and files needed to authorize the key
         """
         if self.user == "root":
             __directory = "/" + self.user + "/.ssh/"
@@ -116,13 +145,17 @@ class SSHAuthorizedKeys:
             __directory = "/home/" + self.user + "/.ssh/"
 
         if not os.path.exists(__directory):
+            print_console("Creating the folder: " + __directory)
             os.makedirs(__directory, 0o700, exist_ok=True)
 
         __authorized_keys_file = __directory + "authorized_keys"
         __file_exists = os.path.isfile(__authorized_keys_file)
+
         if not __file_exists:
+            print_console("Creating the file: " + __authorized_keys_file)
             open(__authorized_keys_file, "w")
 
+        print_console("Define rights")
         os.chmod(__authorized_keys_file, 0o600)
         subprocess_run(["chown", self.user + ":" + self.user, __directory])
         subprocess_run(["chown", self.user + ":" + self.user, __authorized_keys_file])
@@ -131,130 +164,229 @@ class SSHAuthorizedKeys:
 
     def authorize_key(self):
         """
-        TODO
-        :return:
+        Create a temporary directory to download the key and use it in the created file.
+        :return: Path of the file containing the key
         """
         with tempfile.TemporaryDirectory() as directory:
+            print("The created temporary directory is" + directory)
             __ssh_pub_key_filename = self.__download_key(directory)
             __ssh_pub_key_path = directory + "/" + __ssh_pub_key_filename
-
             __ssh_key_str = self.read_file(__ssh_pub_key_path)
+
+            print_console("Allow the key: " + __ssh_key_str)
             self.put_key(self.key_path_user, __ssh_key_str)
             return __ssh_pub_key_path
 
     def __download_key(self, directory):
         """
-        TODO
-        :param directory:
-        :return:
+        Download the key from a defined URL and place it in the folder and return the file name.
+        :param directory: File where the file containing the key should be placed
+        :return: Name of the file containing the key
         """
-        print('The created temporary directory is %s' % directory)
+        print_console("Downloading the key: " + self.key_url + " in the folder:" + directory)
         wget.download(self.key_url, directory, bar=None)
         __ssh_pub_key_filename = wget.detect_filename(url=self.key_url)
         return __ssh_pub_key_filename
 
     def read_file(self, file):
         """
-        TODO
-        :param file:
-        :return:
+        Read a file
+        :param file: File to read
+        :return: File contents
         """
         with open(file, 'r') as filehandle:
             return filehandle.read()
 
     def put_key(self, to_file, key_str):
         """
-        TODO
-        :param to_file:
-        :param key_str:
+        Write the key in a file
+        :param to_file: File where the key should be written
+        :param key_str: Key to be written
         """
         with open(to_file, "a") as file:
             file.write("\n" + key_str)
 
     def configure_sshd_config(self):
         """
-        TODO
+        Configure the ssh to be able to connect using our private key
         """
+        print_console("Setting the SSH configuration")
+
         with open("/etc/ssh/sshd_config", 'r+') as f:
             data = f.read()
             data = re.sub(r'(^[#\s]*PubkeyAuthentication.*$)', r'PubkeyAuthentication yes', data, flags=re.MULTILINE)
             data = re.sub(r'(^[#\s]*PermitEmptyPasswords.*$)', r'PermitEmptyPasswords no', data, flags=re.MULTILINE)
-            data = re.sub(r'(^[#\s]*PasswordAuthentication.*$)', r'PasswordAuthentication yes', data,
+            data = re.sub(r'(^[#\s]*PasswordAuthentication.*$)', r'PasswordAuthentication no', data,
                           flags=re.MULTILINE)
-            data = re.sub(r'(^[#\s]*PermitRootLogin.*$)', r'PermitRootLogin yes', data, flags=re.MULTILINE)
+            data = re.sub(r'(^[#\s]*PermitRootLogin.*$)', r'PermitRootLogin no', data, flags=re.MULTILINE)
             f.seek(0)
             f.truncate()
             f.write(data)
-        # TODO: PasswordAuthentication yes and PermitRootLogin yes just for DEBUG
+
         self.restart_sshd()
 
     def restart_sshd(self):
         """
-        TODO
+        Restart ssh service
         """
+        print_console("Restarting the ssh service")
         subprocess_run(["service", "ssh", "restart"])
 
 
 class Apache2:
     """
-    TODO
+    Manage Apache2
     """
 
     def __init__(self, user):
         self.user = user
         self.user_www_dir = None
+        self.domain = user
+        self.document_root = "/home/" + self.user + "/public_html"
+
+    def install(self):
+        """
+        Install Apache2
+        """
+        print_console("Installing apache2")
+        return subprocess_run(["apt-get", "-y", "install", "apache2"])
+
+    def reload(self):
+        """
+        Reload Apache2 configurations
+        """
+        print_console("Reloading the apache2 configuration")
+        return subprocess_run(["service", "apache2", "reload"])
+
+    def enable_conf(self, name):
+        """
+        Enables an Apache2 configuration
+        :param name: Name of the configuration to activate
+        """
+        print_console("Enabling vhost: " + name + ".conf")
+        return subprocess_run(["a2ensite", name + ".conf"])
+
+    def __create_dir(self):
+        """
+        Define the path containing the site's files, create it if it does not exist
+        :return: Folder path containing the site files
+        """
+        if self.user == "root":
+            self.document_root = "/var/www/html"
+        else:
+            self.document_root = "/home/" + self.user + "/public_html"
+
+        # Create the folder if it does not exist
+        if not os.path.exists(self.document_root):
+            print_console("Creating the folder: " + self.document_root)
+            os.makedirs(self.document_root, 0o750, exist_ok=True)
+
+        return self.document_root
+
+    def __set_permissions(self, folder):
+        """
+        Set permissions on the folder
+        :param folder: Folder containing the site files
+        """
+        print_console("Set rights on the folder: " + folder)
+        subprocess_run(["chown", "-R", self.user + ":www-data", folder])
+        subprocess_run(["chmod", "750", "-R", folder])
+
+    def put_default_index(self):
+        """
+        Create an index.html file that contains default content
+        """
+        print_console("Creating the default index.html file")
+        __index_html = """<!doctype html>
+                <html lang="fr">
+                <head>
+                  <meta charset="utf-8">
+                  <title>OCP6 - %s</title>
+                </head>
+                <body>
+                <center><h1 style="color:#7451EB;">Welcome to the <b>%s</b> website</h1></center>
+                </p>
+                <center>
+                <h5><b>OCP6</b></h5>
+                <img src="https://openclassrooms.com/fav-icon.png?v=3"></center>
+                </body>
+                </html>""" % (self.domain, self.domain)
+
+        __index_html_file = self.document_root + "/index.html"
+        with open(__index_html_file, "w") as file:
+            file.write(__index_html)
+
+
+    def create_vhost(self, domain, folder):
+        """
+        Write the vhost file with the defined parameters
+        """
+        print_console("Creation of the vhost: " + domain + ".conf")
+        __virtual_host = """<VirtualHost *:80>
+            ServerAdmin admin@%s
+            ServerName %s
+            ServerAlias www.%s
+            DocumentRoot %s
+
+            ErrorLog ${APACHE_LOG_DIR}/error.log
+            CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+            <Directory %s>
+                DirectoryIndex index.html
+                <Limit POST GET OPTIONS>
+                    Require all granted
+                </Limit>
+                <LimitExcept POST GET OPTIONS>
+                    Require all denied
+                </LimitExcept>
+            </Directory>
+        </VirtualHost>
+                """ % (domain, domain, domain, folder, folder)
+
+        __conf_file = "/etc/apache2/sites-available/" + domain + ".conf"
+        with open(__conf_file, "w") as file:
+            file.write(__virtual_host)
 
     def create(self, domain):
-
-        if self.user == "root":
-            __document_root = "/var/www/html"
-        else:
-            __document_root = "/home/" + self.user + "/public_html"
-
-        if not os.path.exists(__document_root):
-            os.makedirs(__document_root, 0o750, exist_ok=True)
-
-        subprocess_run(["chown", "-R", self.user + ":www-data", __document_root])
-        # subprocess_run(["usermod", "-a", "-G", "www-data", self.user])
-        subprocess_run(["chmod", "750", "-R", __document_root])
-
-        os.system("""echo "<VirtualHost *:80>
-    ServerAdmin """ + domain + """
-    ServerName """ + domain + """
-    ServerAlias www.""" + domain + """
-    DocumentRoot """ + __document_root + """
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-    <Directory """ + __document_root + """>
-    DirectoryIndex index.html
-    <Limit POST GET OPTIONS>
-    Require all granted
-    </Limit>
-    <LimitExcept POST GET OPTIONS>
-    Require all denied
-    </LimitExcept>
-    </Directory>
-</VirtualHost>" >> /etc/apache2/sites-available/""" + domain + """.conf
-        """)
-
-        subprocess_run(["a2ensite", domain + ".conf"])
-        subprocess_run(["service", "apache2", "reload"])
+        """
+        Create the necessary to host the files of the site
+        :param domain: Domain name
+        """
+        self.domain = domain
+        __document_root = self.__create_dir()
+        self.put_default_index()
+        self.__set_permissions(__document_root)
+        self.create_vhost(domain, __document_root)
+        self.enable_conf(domain)
+        self.reload()
 
 
+print_console("START OF SCRIPT")
 subprocess_run(["apt-get", "update"])
 subprocess_run(["apt-get", "-y", "install", "python3-pip"])
 
-super_pip(['wget'])  # pip install wget
+super_pip(['wget', 'ifaddr'])  # pip install wget
 
-test_usr = User(SSH_USER, None)
-test_usr.create()
+user = User(SSH_USER, None)
+user.create()
 
-test_ssh = SSHAuthorizedKeys(test_usr.user, URL_SSH_PUB_KEY)
-test_ssh.create_dir_and_file()
-test_ssh.authorize_key()
-test_ssh.configure_sshd_config()
+ssh = SSHAuthorizedKeys(user.user, URL_SSH_PUB_KEY)
+ssh.create_dir_and_file()
+ssh.authorize_key()
+ssh.configure_sshd_config()
 
-subprocess_run(["apt-get", "-y", "install", "apache2"])
+apache = Apache2(user.user)
+apache.install()
+apache.create(user.user)
 
-test_apache = Apache2(test_usr.user)
-test_apache.create(test_usr.user)
+print_console("END OF THE SCRIPT")
+print_console("Machine information")
+print("* Network interfaces: ")
+adapters = ifaddr.get_adapters()
+
+for adapter in adapters:
+    print(adapter.nice_name)
+    for ip in adapter.ips:
+        print("   %s/%s" % (ip.ip, ip.network_prefix))
+print("* Session name: " + user.user)
+print("* Use your private key to connect")
